9944378e19d34b845dc0cb5969af50ff07b63662
ARTR <trejoramon141295@gmail.com>
2023-03-27 16:57:32 -0600

    Se crearon los archivos

---
+++ JS-04 - Arrays and Loops/Alberto Trejo/Ejercicio 1.1/js/main.js
@@ -0,0 +1,3 @@
+console.log("********Ejercicio 1**********");
+let libros = ["Sofia","David","Juan"];
+let divAlert = document.getElementById("divEjercicio1");
---
+++ JS-04 - Arrays and Loops/Alberto Trejo/Ejercicio 2/js/main.js
@@ -0,0 +1,26 @@
+let cont =0; //cont para todos
+let cont2=0;
+
+console.log("******Ejercicio 2********");
+for (let cont = 0; cont <= 5; cont++) {
+    for (let cont2 = 0; cont2 <=5; cont2++) {
+                
+    }//for cont2    
+}//for cont
+
+// console.log("******while********"); 
+// cont =0;
+// while (cont < 100) {
+//     console.log(cont);
+//     cont+=5;    
+// }//while
+
+// console.log("********Do while**********");
+// cont = 0; 
+// do {
+//     console.log(cont); 
+//     cont+=5
+// } while (cont<100);
+// //dowhile
+
+
