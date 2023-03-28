5c7eaeb428d0bcbf3737c8472d35061e69d96867
ARTR <trejoramon141295@gmail.com>
2023-03-27 19:03:47 -0600

    Se termino ejercicio 2

--- JS-04 - Arrays and Loops/Alberto Trejo/Ejercicio 2/js/main.js
+++ JS-04 - Arrays and Loops/Alberto Trejo/Ejercicio 2/js/main.js
@@ -1,26 +1,11 @@
-let cont =0; //cont para todos
-let cont2=0;
+let filas =0; 
 
 console.log("******Ejercicio 2********");
-for (let cont = 0; cont <= 5; cont++) {
-    for (let cont2 = 0; cont2 <=5; cont2++) {
-                
-    }//for cont2    
-}//for cont
 
-// console.log("******while********"); 
-// cont =0;
-// while (cont < 100) {
-//     console.log(cont);
-//     cont+=5;    
-// }//while
-
-// console.log("********Do while**********");
-// cont = 0; 
-// do {
-//     console.log(cont); 
-//     cont+=5
-// } while (cont<100);
-// //dowhile
-
-
+for ( filas = 1; filas <= 5; filas++) {
+    let asteriscos ="";
+    for ( let cont = 1; cont<=filas; cont++) { 
+        asteriscos += "* ";            
+    }//for filas
+    console.log(asteriscos); 
+}//for 
